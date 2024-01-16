b5c73ee531a4360e18e1e47ec8f688c299382e3f
Jyoti190512 <134208281+Jyoti190512@users.noreply.github.com>
2024-01-16 01:11:48 +0100

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
 // write js code here if required
+function updateTimer() {
+    const timerElement = document.getElementById('timer');
+    const currentDate = new Date();
+
+    const day = currentDate.getDate();
+    const month = currentDate.getMonth() + 1; // Adding 1 since months are zero-based
+    const year = currentDate.getFullYear();
+
+    const hours = currentDate.getHours();
+    const minutes = currentDate.getMinutes();
+    const seconds = currentDate.getSeconds();
+
+    const formattedTimer = `${day}/${month}/${year}, ${hours}:${minutes}:${seconds}`;
+
+    timerElement.textContent = formattedTimer;
+}
+
+// Update the timer every second
+setInterval(updateTimer, 1000);
+
+// Initial update
+updateTimer();
+
